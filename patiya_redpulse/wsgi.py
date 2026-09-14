fe442b6feac5490e41c5178396e10b409ed3bee6
Junayet Islam <junayetislam540@gmail.com>
2026-09-14 17:12:31 +0600

    Update wsgi.py

--- patiya_redpulse/wsgi.py
+++ patiya_redpulse/wsgi.py
@@ -4,3 +4,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'patiya_redpulse.settings')
 
 application = get_wsgi_application()
+app = application
